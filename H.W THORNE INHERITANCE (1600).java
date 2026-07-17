1766fb4dc243ca45cb6e14c69d0328c00223791b
syed212657-ctrl <syed212657@gmail.com>
2026-07-17 14:43:16 +0530

    Create H.W THORNE INHERITANCE (1600).java

---
+++ H.W THORNE INHERITANCE (1600).java
@@ -0,0 +1,39 @@
+class ThroneInheritance {
+
+    private String king;
+    private Map<String, List<String>> family;
+    private Set<String> dead;
+
+    public ThroneInheritance(String kingName) {
+        king = kingName;
+        family = new HashMap<>();
+        dead = new HashSet<>();
+        family.put(kingName, new ArrayList<>());
+    }
+
+    public void birth(String parentName, String childName) {
+        family.get(parentName).add(childName);
+        family.put(childName, new ArrayList<>());
+    }
+
+    public void death(String name) {
+        dead.add(name);
+    }
+
+    public List<String> getInheritanceOrder() {
+        List<String> order = new ArrayList<>();
+        dfs(king, order);
+        return order;
+    }
+
+    private void dfs(String person, List<String> order) {
+        if (!dead.contains(person)) {
+            order.add(person);
+        }
+
+        for (String child : family.get(person)) {
+            dfs(child, order);
+        }
+    }
+}
+
