7088b11816f52868031c196637a78b1d1e6e8f17
syed212657-ctrl <syed212657@gmail.com>
2026-07-17 14:41:34 +0530

    Create H.W DIVIDE TWO INTEGERS (29).java

---
+++ H.W DIVIDE TWO INTEGERS (29).java
@@ -0,0 +1,34 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        // Handle overflow
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        int result = 0;
+
+        while (dvd >= dvs) {
+            long temp = dvs;
+            int multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+            result += multiple;
+        }
+
+        // Apply the correct sign
+        if ((dividend < 0) ^ (divisor < 0)) {
+            result = -result;
+        }
+
+        return result;
+    }
+}
